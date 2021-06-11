b772d6232db931115d14e4ebef9820e5752dc412
Adib Legastelois <adiblegastelois@MacBook-Air-de-Adib.local>
2021-06-11 17:42:46 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# First things to do before coding
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+
+
+# 2 import object Widget + Use Kv file
+class MainWidget(Widget):
+    pass
+
+
+class BoxLayoutExemple(BoxLayout):
+    pass
+
+
+# 3 Create our Object
+class LeLabApp(App):
+    pass
+
+
+# 4 run our App
+LeLabApp().run()
