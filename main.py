00aa8fd8067c4f1f43d723ad1e13f8068a1365f7
Adib Legastelois <adiblegastelois@MacBook-Air-de-Adib.local>
2021-06-11 19:03:04 +0100

    Unable or Disable Button dependin of BooleanProperty

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 # First things to do before coding
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, BooleanProperty
 
 
 # 2 import object Widget + Use Kv file
-class WidgetsExemple(GridLayout):
-    my_text = StringProperty("Cool")
+class WidgetsExample(GridLayout):
     count = 0
+    activ_counter = BooleanProperty(False)
+    my_text = StringProperty("Cool")
+
     def on_button_click(self):
-        self.count += 1
-        self.my_text = str(self.count)
+        if self.activ_counter:
+            self.count += 1
+            self.my_text = str(self.count)
+
+
+    def on_toggle_state(self, widget):
+        # print(widget.state)
+        if widget.state == "down":
+            print("Man down")
+            widget.text = "ON"
+            self.activ_counter = True
+        else:
+            print("Man normal state is alive")
+            widget.text = "OFF"
+            self.activ_counter = False
+
+
+
+
 
 
 # 3 Create our Object
